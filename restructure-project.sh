a5206f6e290d6e01281db3de578e7e692018459f
james7132 <james.siqi.liu@gmail.com>
2018-01-03 08:04:42 -0800

    Update restructure project script

--- restructure-project.sh
+++ restructure-project.sh
@@ -2,16 +2,26 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SRC_ASSETS_DIR=src
+SRC_TESTS_DIR=test
 SRC_PROJECT_SETTINGS_DIR="$SCRIPT_DIR/ProjectSettings"
 
-DST_ASSETS_DIR=./Project/Assets
+DST_ASSETS_DIR=./Project/Assets/src
+DST_TEST_DIR=./Project/Assets/test
 DST_PROJECT_SETTINGS_DIR=./Project/ProjectSettings
 TARGET_PROJECT_SETTINGS_DIR
 
-mkdir -p $DST_ASSETS_DIR
-mkdir -p $DST_PROJECT_SETTINGS_DIR
+copy() {
+  if [ -d $1 ]; then
+    mkdir -p $2
+    cp -r $1 $2
+    echo "Copied $1 to $2"
+  else
+    echo "Directory $1 does not exist. Skipping..."
+  fi
+}
 
-cp -r $SRC_ASSETS_DIR $DST_ASSETS_DIR
-echo "Copied $SRC_ASSETS_DIR to $DST_ASSETS_DIR"
-cp -r $SRC_PROJECT_SETTINGS_DIR $DST_PROJECT_SETTINGS_DIR
-echo "Copied $SRC_PROJECT_SETTINGS_DIR to $DST_PROJECT_SETTINGS_DIR"
+copy $SRC_ASSETS_DIR $DST_ASSETS_DIR
+copy $SRC_TEST_DIR $DST_TEST_DIR
+copy $SRC_PROJECT_SETTINGS_DIR $DST_PROJECT_SETTINGS_DIR
+
+echo 'Finished restructuring project.'
