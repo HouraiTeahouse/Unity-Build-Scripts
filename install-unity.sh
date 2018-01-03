8942ad010e1f919a8f2d77ee5aaaa220c0d80fbc
james7132 <james.siqi.liu@gmail.com>
2018-01-03 11:26:43 -0800

    Fix typo

--- install-unity.sh
+++ install-unity.sh
@@ -13,7 +13,7 @@ install() {
 
   echo "Installing $package to $install_dir"
   chmod +x $package
-  ./$package --unattended --instal-location="$install_dir" --list-components
+  ./$package --unattended --install-location="$install_dir" --list-components
 
   ls $install_dir
 }
